91b131f7d10fbafee26c5c5a48d2815124387647
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-10-28 12:34:10 -0300

    initialize component login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
+import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>INITIAL PROJECT</h1>
-    </>
-    
+    <div>
+      <Switch>
+        <Route path="/" exact component={Login} />
+      </Switch>
+    </div>
     )
 }
 
