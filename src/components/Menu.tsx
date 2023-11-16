ddaa9bc4398bfa91e402b62f247d4b17aef121c6
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-11-16 17:20:53 -0300

    fix: lint

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import "../menu.css"
 import { getCategories, getProductsFromCategoryAndQuery } from '../services/fetch'
 import { useItemsContext } from '../context/ItemsContext';
